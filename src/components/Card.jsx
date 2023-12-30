d630aa073c05d38dac5448c8853af78b06199e0c
Alfonso Raggio <projectja@gmail.com>
2023-12-31 00:17:38 +0100

    sexto commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 //    {empresa[0].author}
 export default function Card({empresa}) {
-    console.log("la empresa en card es interesante . cuarto commit " + empresa[0].author)
+    console.log("la empresa en card es interesante . quinto commit " + empresa[0].author)
   return (    
     <div>
         <p>
